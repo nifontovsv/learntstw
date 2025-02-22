783b0cd99eb0551c7f66404c02b9e86a3494f090
nifontovsv <nifontovxv@gmail.com>
2025-02-22 22:41:43 +0300

    todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
+import AddTodoForm from './components/AddTodoForm';
+import TodoList from './components/TodoList';
+import TodoSummary from './components/TodoSummary';
+import useTodos from './hooks/useTodos';
+
 function App() {
+	const { todos, setTodoCompleted, deleteAllCompleted, deleteTodo, addTodo } = useTodos();
+
 	return (
-		<>
-			<div className='flex bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5'>
-				<div>
-					<span className='inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg'>
-						<svg className='h-6 w-6 stroke-white'></svg>
-					</span>
-				</div>
-				<h3 className='text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight '>
-					Writes upside-down
-				</h3>
-				<p className='text-gray-500 dark:text-gray-400 mt-2 text-sm '>
-					The Zero Gravity Pen can be used to write in any orientation, including upside-down. It
-					even works in outer space.
-				</p>
+		<main className='py-10 h-screen space-y-5 overflow-y-auto'>
+			<h1 className='font-bold text-3xl text-center'>Your Todo</h1>
+			<div className='max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6'>
+				<AddTodoForm onSubmit={addTodo} />
+				<TodoList todos={todos} onCompletedChange={setTodoCompleted} onDelete={deleteTodo} />
 			</div>
-			<h1 className='text-3xl font-bold underline decoration-amber-500'>Hello world!</h1>
-		</>
+			<TodoSummary todos={todos} deleteAllCompleted={deleteAllCompleted} />
+		</main>
 	);
 }
 
---
+++ src/data/todos.ts
@@ -0,0 +1,7 @@
+import { Todo } from '../types/todo';
+
+export const dummyData: Todo[] = [
+	{ id: 1, title: 'Like this video', completed: false },
+	{ id: 2, title: 'Subscribe to coding', completed: false },
+	{ id: 3, title: 'Leave a nice comment', completed: false },
+];
